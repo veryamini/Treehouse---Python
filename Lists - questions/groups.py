529f35b5846c840096d63b5e59fe3202a7057dad
Yamini Verma <vermayamini13@gmail.com>
2018-06-29 19:41:26 +0530

    Lists course questions

---
+++ Lists - questions/groups.py
@@ -0,0 +1,16 @@
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+# Your code here
+group_number = 1
+for group in musical_groups:
+    if len(group) == 3:
+        print("* Group #{}: {} ".format(group_number, ", ".join(group)))
+        group_number += 1
+    
---
+++ Lists - questions/meeting.py
@@ -0,0 +1,12 @@
+attendees = ["Ken", "Alena", "Treasure"]
+attendees.append("Ashley")
+attendees.extend(["James", "Guil"])
+optional_invitees = ["Ben J.", "Dave"]
+potential_attendees = attendees + optional_invitees
+print("There are", len(potential_attendees), "potential attendees currently")
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To: " + to_line)
+print("CC: " + cc_line)
+
